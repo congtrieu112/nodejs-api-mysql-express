bb4dd91c0ed8161d81eaad273cd1486ee2c63c46
Congtrieu <ittrjeu@gmail.com>
2019-07-08 01:15:07 +0700

    update config

--- lib/app.ts
+++ lib/app.ts
@@ -3,7 +3,7 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import { Routes } from "./routes/crmRoutes";
 import * as mongoose from "mongoose";
-import {createConnection} from "typeorm";
+import { createConnection } from "typeorm";
 // import { Clients } from "./entity/clients";
 import { Users } from "./entity/Users";
 import { join } from "path";
@@ -12,50 +12,50 @@ class App {
 
     public app: express.Application = express();
     public routePrv: Routes = new Routes();
-    public mongoUrl: string = 'mongodb://45.63.0.193:27017/CRMdb';  
+    public mongoUrl: string = 'mongodb://45.63.0.193:27017/CRMdb';
     // public mongoUrl: string = 'mongodb://dalenguyen:123123@localhost:27017/CRMdb';
 
     constructor() {
         this.config();
         this.mongoSetup();
-        this.routePrv.routes(this.app);  
-        this.mysqlSetup();   
+        this.routePrv.routes(this.app);
+        this.mysqlSetup();
     }
 
-    private config(): void{
+    private config(): void {
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: false }));
         // serving static files 
         this.app.use(express.static('public'));
-        this.app.use((req, res, next) =>{
+        this.app.use((req, res, next) => {
             res.header("Access-Control-Allow-Origin", "*");
             res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
             next();
-          });
+        });
     }
 
-    private mongoSetup(): void{
+    private mongoSetup(): void {
         mongoose.Promise = global.Promise;
-        mongoose.connect(this.mongoUrl, {useNewUrlParser: true});        
+        mongoose.connect(this.mongoUrl, { useNewUrlParser: true });
     }
 
-    private mysqlSetup(): void{
+    private mysqlSetup(): void {
         createConnection({
             type: "mysql",
             host: "localhost",
             port: 3306,
             username: "root",
-            password: "",
+            password: "@1Qwerty2345",
             database: "quanly",
             entities: [
                 __dirname + "/entity/*.ts"
             ],
             synchronize: true,
             logging: true,
-            charset:"utf8mb4_general_ci"
+            charset: "utf8mb4_general_ci"
         }).then(async connection => {
-            console.log('connected',__dirname)
-           
+            console.log('connected', __dirname)
+
         })
     }
 
