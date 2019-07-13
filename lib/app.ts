fd5204b61e8807cec8020339871dedbd838a7202
Congtrieu <ittrjeu@gmail.com>
2019-07-13 22:24:54 +0700

    update

--- lib/app.ts
+++ lib/app.ts
@@ -29,6 +29,7 @@ class App {
         this.app.use(express.static('public'));
         this.app.use((req, res, next) => {
             res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Methods", "*");
             res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
             next();
         });
